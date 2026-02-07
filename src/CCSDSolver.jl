f7f2c2df1c56a6567ff5999fed65dda2bf004ba8
RTChou67 <3023207450@tju.edu.cn>
2026-02-07 16:39:36 +0800

    fix rccsd

--- src/CCSDSolver.jl
+++ src/CCSDSolver.jl
@@ -1,11 +1,11 @@
 export CCSDResults
 
 struct CCSDResults
-    ENum::Int
-    nocc::Int
-    nvir::Int
-    t1::Matrix{Float64}
-    t2::Array{Float64, 4}
-    Ecc::Float64
-    Etot::Float64
+	ENum::Int
+	nocc::Int
+	nvir::Int
+	t1::Matrix{Float64}
+	t2::Array{Float64, 4}
+	Ecc::Float64
+	Etot::Float64
 end
